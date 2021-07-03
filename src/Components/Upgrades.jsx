8b7aebca3b268055b1af5e0ff22c5114124e1bcf
pranshuteotia <pranshu28@gmail.com>
2021-07-02 20:59:00 -0400

    Implement upgrades component, and manual save system.

--- src/Components/Upgrades.jsx
+++ src/Components/Upgrades.jsx
@@ -1,6 +1,16 @@
-const Upgrades = () => {
+import Options from "./Options";
+import '../css/Upgrades.css'
+
+const Upgrades = ( { saveGame }) => {
     return(
         <div className="upgrades">
+            <Options saveGame={saveGame} />
+
+            <h3>Upgrades</h3>
+
+            <div className="upgrades-container">
+                
+            </div>
         </div>
     );
 }
